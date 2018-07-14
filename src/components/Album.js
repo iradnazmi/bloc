915d360b63abd8a51f375398432e4f988ac3c9f9
Irad Nazmi <irad.h.nazmi@gmail.com>
2018-07-14 10:14:01 -0400

    Assignment 15 Audio Playback: typo fixed

--- src/components/Album.js
+++ src/components/Album.js
@@ -60,7 +60,7 @@ class Album extends Component {
       <section className="album">
         <section id="album-info">
           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-          <div classname="album-details">
+          <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
             <div id="release-info">{this.state.album.releaseInfo}</div>
